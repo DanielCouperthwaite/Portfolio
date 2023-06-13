49b5d5b131073b5e4d31ecc35cbe57b18c15d2bd
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-06-13 21:45:16 +0100

    added contact form functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,30 @@ import React from "react";
 import Details from "./Details";
 
 export default function Contact() {
+
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message }),
+    })
+      .then(() => alert("Message sent!"))
+      .catch((error) => alert(error));
+  }
+
   return (
     <section id="contact" className="relative">
        <div className="bg-fuchsia-800 py-24 sm:py-32">
