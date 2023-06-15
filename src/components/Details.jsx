ddbf7543e8b036d64c047d660f16de18d9bbf4d0
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-06-15 19:11:05 +0100

    check functionality for mobile and tablet screens

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -9,20 +9,20 @@ export default function Details () {
         <dl class="divide-y divide-gray-100">
           <div class="px-4 py-10 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt class="text-sm font-medium leading-6 text-gray-900">Full name</dt>
-            <dd class="mt-1 text-xl leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Daniel Couperthwaite</dd>
+            <dd class="mt-1 text:sm md:text-xl leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Daniel Couperthwaite</dd>
           </div>
       
           <div class="px-4 py-10 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt class="text-sm font-medium leading-6 text-gray-900">Email address</dt>
-            <dd class="mt-1 text-xl leading-6 text-gray-700 sm:col-span-2 sm:mt-0">danielcoup1@outlook.com</dd>
+            <dd class="mt-1 text:sm md:text-xl leading-6 text-gray-700 sm:col-span-2 sm:mt-0">danielcoup1@outlook.com</dd>
           </div>
           <div class="px-4 py-10 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt class="text-sm font-medium leading-6 text-gray-900">Phone</dt>
-            <dd class="mt-1 text-xl leading-6 text-gray-700 sm:col-span-2 sm:mt-0">07434643104</dd>
+            <dd class="mt-1 text:sm md:text-xl leading-6 text-gray-700 sm:col-span-2 sm:mt-0">07434643104</dd>
           </div>
           <div class="px-4 py-10 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt class="text-sm font-medium leading-6 text-gray-900">Location</dt>
-            <dd class="mt-1 text-xl leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Manchester</dd>
+            <dd class="mt-1 text:sm md:text-xl leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Manchester</dd>
           </div>
           
           
