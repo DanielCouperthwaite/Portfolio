da38bb60cbab7c22884a0260f6c1488a24ab2f6d
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-06-17 15:27:47 +0100

    edit contact section

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -22,7 +22,7 @@ export default function Details () {
           </div>
           <div class="px-4 py-10 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt class="text-sm font-medium leading-6 text-gray-900">Location</dt>
-            <dd class="mt-1 text:sm md:text-xl leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Manchester</dd>
+            <dd class="mt-1 text:sm md:text-xl leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Manchester, Open to Relocate</dd>
           </div>
           
           
