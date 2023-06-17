5b6f10eefd430181cd2789d528617d18defdcda6
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-06-17 14:54:54 +0100

    fix spacing issue with skills language section

--- src/components/TechnicalSkills.jsx
+++ src/components/TechnicalSkills.jsx
@@ -86,7 +86,7 @@ export default function TechnicalSkills() {
           if(currentField === 'lang'){
             return (
             <>
-            <dl className="grid max-w-xl grid-cols-2 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-3 lg:gap-y-16">
+            <dl className="grid max-w-xl sm:grid-cols-2 grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-3 lg:gap-y-16">
             {languages.map((feature) => (
               <div key={feature.name} className="relative pl-16">
                 <dt className="text-lg py-2 px-1 font-semibold leading-10 text-white">
