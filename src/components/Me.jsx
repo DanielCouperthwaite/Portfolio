f8d37bb57618156d0141af274c1df086399bb0ef
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-08-29 09:31:29 +0100

    change about me description

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -2,31 +2,32 @@ export default function Me () {
     return (
         <>
             <div className="container mx-auto max-w-7xl px-6 lg:px-4 py-20">
-                <div className="mx-auto max-w-2xl lg:text-justify">
+                <div className="mx-auto max-w-2xl">
           
                     <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
                          About Me
                     </p>
                     <p className="mt-6 text-lg leading-8">
-                        For the last five years, I have been teaching English as a second language to students in 
-                        Europe, Asia and South America. I worked in Vietnamese public schools in Gò Vấp, Japanese universities around Tokyo, and online from just about everywhere else! The youngest students I taught were just 3 years old, and the oldest was 79! 
-                        (He was frustrated his grandchildren could communicate in English without him knowing!) 
+                        My name is Daniel, an English teacher turned full stack developer.
                     </p>
-                    <p className="mt-6 text-lg leading-8"> 
-                        Now, back in the UK, I have been learning to code since 
-                        the start of this year as a way to bring about some stability to my life. I love the problem-solving aspects, and the ability to collaborate
-                        with others to build something much larger than I could have done alone.
+                    <p className="mt-6 text-justify text-lg leading-8">
+                        The youngest students I taught were just 3 years old, and the oldest was 79! ( He was frustrated his grandchildren could communicate in English without him knowing! )
                     </p>
-                    <p className="mt-6 text-lg leading-8"> 
-                        In my free time you can find me either on top of a mountain in the Lake or Peak district. I have been playing guitar since I was a teenager and also love trying out new recipes. (I am so proud of 
-                        my homemade naan breads I even listed them as a bonus project below!)
+                    <p className="mt-6 text-lg leading-8">
+                        I am growing my development skills every day and would love the chance to do this in a team as a junior developer. You can see some of the projects I have made below, or follow updates from my linkedIn account.
                     </p>
-                    <p className="mt-6 text-lg leading-8"> 
-                        Feel free to get in touch if you have any questions about myself, my projects or you have recipes to recommend!
+                    <p className="mt-6 text-lg leading-8">
+                        In my free time you can find me either on top of a mountain in the Lake or Peak district. I have been playing guitar since I was a teenager and also love trying out new recipes. (I am so proud of my homemade naan breads I even listed them as a bonus project below!)
                     </p>
-                    <p className="mt-6 text-lg leading-8"> 
+                    <p className="mt-6 text-lg leading-8">
+                        Feel free to get in touch if you have any questions about myself, my projects or you have recipes to recommend.
+                    </p>
+                    <p className="mt-6 text-lg leading-8">
                         Thanks for stopping by!
                     </p>
+                        
+                        
+                        
                 </div>
           </div>
         </>
