0cd133157f9e148765bae162abb0509aea65bc77
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-09-11 15:50:51 +0100

    alter image sizes(3)

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -211,7 +211,7 @@ export default function Projects() {
         </div>
         <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
           <a target="_blank" href="https://just-news8118.netlify.app/"><img
-            className="lg:w-[30rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[40rem]  w-[21rem]"
+            className="lg:w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[40rem] w-[21rem]"
             src={just}
             alt="A screenshot of the Just News home page"
 
