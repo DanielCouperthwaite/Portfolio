a3b5417defd356e0126b3658a3dc83987da8a1bd
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2024-06-23 10:37:13 +0100

    spiking formsubmit(2)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -44,7 +44,7 @@ export default function Contact() {
           <Details />
         </div>
         <form
-          action="https://formsubmit.co/your@email.com"
+          action="https://formsubmit.co/danielcoup1@outlook.com"
           name="contact"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
